2265f938c284fd7f679428c3866304abca93adcf
raneemabuhasan <raneem.abuhasan001@umb.edu>
2026-04-06 16:41:53 -0400

    Added new hovering effects to the about section

--- js/about.js
+++ js/about.js
@@ -1,4 +1,14 @@
-const page = document.body.dataset.page;
+/**const page = document.body.dataset.page;
 document.querySelectorAll("[data-link]").forEach(a => {
   if (a.dataset.link === page) a.classList.add("active");
+});
+**/
+const page = document.body.dataset.page;
+
+document.querySelectorAll("[data-link]").forEach(a => {
+  if (a.dataset.link === page) a.classList.add("active");
+});
+
+document.querySelectorAll(".info-box").forEach((box, index) => {
+  box.style.animationDelay = `${index * 0.15}s`;
 });
