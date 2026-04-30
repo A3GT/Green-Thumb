582268f7dce7f508754dca75bd0be8f1cd852bf3
raneemabuhasan <raneem.abuhasan001@umb.edu>
2026-04-30 10:57:05 -0400

    changed settings.py DEBUG back to false

--- backend/greenthumb/settings.py
+++ backend/greenthumb/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "DEV_ONLY_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['cs.umb.edu', '127.0.0.1', 'localhost', '.onrender.com']
 
